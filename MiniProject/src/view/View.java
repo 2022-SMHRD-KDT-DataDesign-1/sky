888257942201fc3c1a1c5f107728bdd8824036d3
cyzlcyzl <cyzlcyzl09@gmail.com>
2023-01-26 19:52:34 +0900

    230126/염혜정/횟수오류수정 및 프린트문 수정

--- MiniProject/src/view/View.java
+++ MiniProject/src/view/View.java
@@ -50,8 +50,8 @@ public class View {
 			}
 		}
 		
-		int gameCnt = 0; // 게임 횟수
 		while(true) {
+			int gameCnt = 0; // 게임 횟수
 			System.out.print("[1]게임시작 [2]랭킹확인 [3]게임종료 >> "); 
 			int select = sc.nextInt();
 			
@@ -60,17 +60,17 @@ public class View {
 				ct.musicEffect(1);
 				System.out.println("============================게임설명===========================");
 				System.out.println(" ① 게임은 3회동안 진행됩니다.\n ② 0점에서 시작하여 선택한 난이도만큼 승리시 점수획득하며 실패시 점수가 줄어들게됩니다.\n"
-						+ " ③ 노래가 재생되는 10초이내 정답입력시 난이도의 2배 점수를 획득합니다.\n④ 정답입력시 띄어쓰기는 불가하며 영어는 소문자만 가능합니다.\n"
+						+ " ③ 노래가 재생되는 10초이내 정답입력시 난이도의 2배 점수를 획득합니다.\n ④ 정답입력시 띄어쓰기는 불가하며 영어는 소문자만 가능합니다.\n"
 						+ " ⑤ 랭킹순위에는 본인의 최고점수만 반영됩니다.");
 				System.out.println("============================================================");
 				// 장르 선택
 				while (true) {
-					System.out.println("장르를 선택하세요 [1]~[4]");
+					System.out.println("장르를 선택하세요");
 					System.out.print("[1]발라드 [2]댄스 [3]동요 [4]POP >> ");
 					int type1 = sc.nextInt();
 					
 					// 난이도 선택
-					System.out.println("난이도를 선택하세요 [1]~[4]");
+					System.out.println("난이도를 선택하세요");
 
 					System.out.print("[1]25 [2]50 [3]75 [4]100 >> ");
 					int point1 = sc.nextInt();
